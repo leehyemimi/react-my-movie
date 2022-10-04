573290e1843a0a6b94e71ff34aa914908826917a
테스트 <test@gmail.com>
2022-10-04 17:14:45 +0900

    완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
 import './App.scss';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Navbar, Container } from 'react-bootstrap';
+import Home from "./routes/Home";
+import Detail from "./components/Detail";
+
 
 function App() {
+	const navigate = useNavigate();
 	return (
-		<div className="App">
-			react-my-movie
-		</div>
+		<>
+			<Navbar bg="light">
+				<Container>
+					<Navbar.Brand onClick={() => { navigate('/') }}>HOME</Navbar.Brand>
+				</Container>
+			</Navbar>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/detail/:id" element={<Detail />} />
+				<Route path="*" element={<div>페이지가 없습니다.</div>} />
+			</Routes>
+		</>
 	);
 }
 
